45f6375bb0f8e579082e7343bb5b527cd02488d6
JackRowntree <jack.rowntree.1@gmail.com>
2023-03-12 18:27:11 +0000

    skeleton of build_database.py

---
+++ build_database.py
@@ -0,0 +1,30 @@
+# Use this script to write the python needed to complete this task
+
+def read_data():
+	pass
+
+def import_api_data():
+	pass
+
+def generate_data():
+	pass
+
+def create_tables():
+	pass
+
+def load_data():
+	pass
+
+def run_poc_sql():
+	pass
+	
+def build_database():
+	read_data()
+	import_api_data()
+	generate_data()
+	create_tables()
+	load_data()
+	run_poc_sql()
+
+if __name__ == __'main'__:
+	build_database()
